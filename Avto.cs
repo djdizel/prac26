bcc4de42795c6a9abb99094b37873efe58466a20
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-02-28 13:06:29 +0300

    123

---
+++ Avto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PRACTICUM26
+{
+    internal class Avto
+    {
+        /// <summary>
+        /// поля
+        /// </summary>
+        string name;
+        int release_date;
+        public static int summary;
+        /// <summary>
+        /// инкапсуляция полей
+        /// </summary>
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Release_date 
+        { 
+            get { return release_date; }
+            set { release_date = value; }
+        }
+        //public static int Summary 
+        //{ 
+        //    get { return summary; }
+        //    set { summary = value; }
+        //}
+        /// <summary>
+        /// конструктор без параметров
+        /// </summary>
+        public Avto() { }
+        /// <summary>
+        /// конструктор с параметрами
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="release_date"></param>
+        public Avto(string name, int release_date)
+        {
+            this.Name = name;
+            this.Release_date = release_date;
+            summary++;
+        }
+        /// <summary>
+        /// Вывод
+        /// </summary>
+        /// <returns></returns>
+        //public override string ToString()
+        //{
+        //    return $"Модель:{Name}\nГод выпуска:{Release_date}\nОбщее кол-во автомобилей:{summary}";
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,8 +9,19 @@ namespace PRACTICUM26
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //Экземпляры классов
+            Avto avto1 = new Avto("Пижо",1999);
+            Avto avto2 = new Avto("БМВ",2007);
+            Avto avto3 = new Avto("Москвич",1998);
+            Avto avto4 = new Avto("ahg",1999);
 
+            Console.WriteLine(avto1.Name,avto1.Release_date);
+            Console.WriteLine(avto2.Name, avto2.Release_date);
+            Console.WriteLine(avto3.Name, avto3.Release_date);
+
+            Console.WriteLine(Avto.summary);
+
+            Console.ReadLine();
         }
     }
 }
